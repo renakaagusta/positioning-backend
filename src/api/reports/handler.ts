7af5b5b8c9ba6f621cae6d5fdb01e7ee9c846642
renaka agusta <renakaagusta@renakas-MacBook-Pro.local>
2022-05-21 22:57:08 +0700

    Update scripts on package.json for heroku deployment

--- src/api/reports/handler.ts
+++ src/api/reports/handler.ts
@@ -8,6 +8,7 @@ export interface ReportHandlerInterface {
     _validator: ReportValidatorInterface;
 
     postReportHandler: (request: any, h: any) => void;
+    putReportHandler: (request: any, h: any) => void;
     getReportsHandler: (request: any, h: any) => void;
     getReportByIdHandler: (request: any, h: any) => void;
     getCalculatedDataHandler: (request: any, h: any) => void;
@@ -21,6 +22,7 @@ class ReportsHandler implements ReportHandlerInterface {
         this._validator = validator;
 
         this.postReportHandler = this.postReportHandler.bind(this);
+        this.putReportHandler = this.putReportHandler.bind(this);
         this.getReportsHandler = this.getReportsHandler.bind(this);
         this.getReportByIdHandler = this.getReportByIdHandler.bind(this);
         this.getCalculatedDataHandler = this.getCalculatedDataHandler.bind(this);
@@ -49,6 +51,63 @@ class ReportsHandler implements ReportHandlerInterface {
             const response = h.response({
                 status: 'success',
                 message: 'Laporan berhasil ditambahkan',
+                data: {
+                    reportId,
+                },
+            });
+
+            response.code(201);
+
+            return response
+        } catch (error) {
+            console.log(error)
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
+            // Server ERROR!
+            const response = h.response({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
+                error: (error as Error).message
+            });
+            response.code(500);
+            return response;
+        }
+    }
+
+    async putReportHandler(request: any, h: any) {
+        try {           
+            this._validator.validateReportPayload(request.payload);
+
+            const { id } = request.params;
+            const { title, description, rider, handler = null, category, createdAt, startingPoint, endPoint, type, status } = request.payload;
+
+            const newReport: ReportInterface = {
+                id: id as string, 
+                title: title as string,
+                description: description as string,
+                category: category as ReportCategory,
+                rider: rider as string,
+                handler: handler as string,
+                status: ReportStatus.Created,
+                startingPoint: startingPoint,
+                endPoint: endPoint,
+                type: type,
+                createdAt: new Date(createdAt)
+            }
+            const oldReport = await this._service.getReportById(id);
+
+            const reportId = await new Promise(async (resolve) => await this._service.updateReport(newReport, oldReport, status).then((reportId: string) => resolve(reportId)))
+
+            const response = h.response({
+                status: 'success',
+                message: 'Laporan berhasil perbarui',
                 data: {
                     reportId,
                 },
--- src/services/firestore/ReportsService.ts
+++ src/services/firestore/ReportsService.ts
@@ -3,7 +3,7 @@ import InvariantError from '../../exceptions/InvariantError';
 import NotFoundError from '../../exceptions/NotFoundError';
 import { connector, route_setup } from '../../helpers/routing';
 import { PointCollectionInterface } from '../../model/pointCollection';
-import { ReportInterface } from '../../model/report';
+import { ReportInterface, ReportStatus } from '../../model/report';
 import { RouteCollectionInterface } from '../../model/routeCollection';
 
 class ReportsService {
@@ -15,18 +15,14 @@ class ReportsService {
 
   async addReport(report: ReportInterface) {
     const pointResult = await this._firestore.collection('points').get()
-
     const pointCollections: Array<PointCollectionInterface> = []
-
     pointResult.docs.map((pointCollection) => pointCollections.push({
       id: pointCollection.id,
       ...pointCollection.data() as PointCollectionInterface
     }))
 
     const routeResult = await this._firestore.collection('routes').get()
-
     const routeCollections: Array<RouteCollectionInterface> = []
-
     routeResult.docs.map((routeCollection) => routeCollections.push({
       id: routeCollection.id,
       ...routeCollection.data() as RouteCollectionInterface
@@ -34,9 +30,7 @@ class ReportsService {
 
     const reportId = await new Promise<string>(async (resolve)=>{
       await connector(routeCollections[0], report.createdAt.getTime()).then(async () => {
-
         const routes = route_setup(report.startingPoint, report.endPoint, pointCollections[0]);
-  
         report.routes = routes
   
         const result = await this._firestore.collection('reports').add(report)
@@ -81,9 +75,7 @@ class ReportsService {
   }
 
   async getCalculatedData() {
-    
     const pointResult = await this._firestore.collection('points').get()
-
     const pointCollections: Array<PointCollectionInterface> = []
 
     pointResult.docs.map((pointCollection) => pointCollections.push({
@@ -92,7 +84,6 @@ class ReportsService {
     }))
 
     const routeResult = await this._firestore.collection('routes').get()
-
     const routeCollections: Array<RouteCollectionInterface> = []
 
     routeResult.docs.map((routeCollection) => routeCollections.push({
@@ -105,6 +96,51 @@ class ReportsService {
     })
 
     return calculatedData
+  }
+
+  async updateReport(newReport: ReportInterface, oldReport: ReportInterface, status: ReportStatus) {
+    const pointResult = await this._firestore.collection('points').get()
+    const pointCollections: Array<PointCollectionInterface> = []
+    pointResult.docs.map((pointCollection) => pointCollections.push({
+      id: pointCollection.id,
+      ...pointCollection.data() as PointCollectionInterface
+    }))
+
+    const routeResult = await this._firestore.collection('routes').get()
+    const routeCollections: Array<RouteCollectionInterface> = []
+    routeResult.docs.map((routeCollection) => routeCollections.push({
+      id: routeCollection.id,
+      ...routeCollection.data() as RouteCollectionInterface
+    }))
+
+    const reportId = await new Promise<string>(async (resolve)=>{
+      await connector(routeCollections[0], newReport.createdAt.getTime()).then(async () => {
+        const routes = route_setup(newReport.startingPoint, newReport.endPoint, pointCollections[0]);
+        newReport.routes = routes
+        
+        if(status == ReportStatus.Rejected) {
+          if(oldReport.rejectedBy != null) {
+            newReport.rejectedBy = [...oldReport.rejectedBy, ...[oldReport.handler!]]
+          } else {
+            newReport.rejectedBy = [...[oldReport.handler!]]
+          }
+          
+          newReport.status = ReportStatus.Created
+        } else {
+          newReport.status = status
+        }
+  
+        const result = await this._firestore.collection('reports').doc(newReport.id!).update(newReport)
+  
+        if (!result) {
+          throw new InvariantError('Report gagal ditambahkan');
+        }
+  
+        resolve(newReport.id!)
+      })
+    })
+
+    return reportId
   }
 }
 
